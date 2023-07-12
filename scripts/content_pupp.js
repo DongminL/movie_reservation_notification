0af90f4c517d4b9a9b1a8822e5f84f9be989d45e
DongminL <milkskfk12@gmail.com>
2023-07-13 01:29:46 +0900

    connet telegram bot

--- scripts/content_pupp.js
+++ scripts/content_pupp.js
@@ -1,6 +1,15 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 const schedule = require('node-schedule');
+const telegram = require("node-telegram-bot-api");
+
+/* Telegram Bot */
+const token = "6331704920:AAEL5bnlVpBKe5Usx0QXQOSOgeLRFrfaD-Y"; // telegram bot token
+const bot = new telegram(token, {polling: true});   // telegram bot api 객체 생성
+const chatId = "6288907835";  // 나의 chat id
+
+let flag = false;   // 해당일의 IMAX관 예매 오픈 여부
+
 
 /* 오늘 날짜 (yyyymmdd) */
 function today() {
@@ -12,6 +21,11 @@ function today() {
     return year + month + day;  // yyyyymmdd 형태로 반환
 }
 
+// Telegram Bot에 메세지 보내기
+function sendMsg(msg) {
+    bot.sendMessage(chatId, msg, {parse_mode: 'Markdown'});
+}
+
 /* 웹 크롤링 */
 async function crawler() {
     try {
@@ -20,7 +34,7 @@ async function crawler() {
             headless: "new"
         });
         const page = await browser.newPage();
-        await page.goto("http://www.cgv.co.kr/theaters/?areacode=01&theaterCode=0013&date=20230726");   // CGV 용산아이파크몰점 예매 사이트 접속
+        await page.goto("http://www.cgv.co.kr/theaters/?areacode=01&theaterCode=0013&date=20230720");   // CGV 용산아이파크몰점 예매 사이트 접속
 
         // 상영시간표 정보가 담긴 iframe으로 전환
         const ifrmHandle = await page.$('iframe[id="ifrm_movie_time_table"]');
@@ -32,16 +46,33 @@ async function crawler() {
 
         // 해당 날짜에 IMAX관 오픈 정보 가져오기
         const imax = $('span.imax');
-        console.log(imax.length)
 
         // IMAX관 오픈 여부에 따른 처리
         if (imax.length > 0) {
-            console.log("IMAX관 오픈");
-            console.log($(imax).parents('.col-times').find('.info-movie > a > strong').text().trim());  // IMAX관에서 상영하는 영화 이름
-            // 상영시간 및 잔여 좌석수
+            let movieNm = $(imax).parents('.col-times').find('.info-movie > a > strong').text().trim(); // IMAX관에서 상영하는 영화 이름
+            let playDate = $(imax).parents('.type-hall').find('.info-timetable > ul > li > a').attr('data-playymd');    // 상영 날짜 (yyyymmdd)
+            let timeTable = ""; // 상영 시간표 및 남은 좌석수
             $(imax).parents('.type-hall').find('.info-timetable > ul > li').each((i, e) => {
-                console.log($(e).text());
+                let startTime = $(e).find('a').attr('data-playstarttime');  // 상영 시작 시간
+                let endTime = $(e).find('a').attr('data-playendtime');  // 상영 종료 시간
+                let seatRemainCnt = $(e).find('a').attr('data-seatremaincnt');  // 남은 좌석수
+                let link = "http://www.cgv.co.kr" + $(e).find('a').attr('href'); // 예매하는 페이지 url
+
+                timeTable += ("\n" + startTime.substring(0,2) + ":" + startTime.substring(2,4) + " ~ " + 
+                            endTime.substring(0,2) + ":" + endTime.substring(2,4) +
+                            " | 남은 좌석 : " + seatRemainCnt + 
+                            " | [예매](" + link + ")");
             });
+
+            console.log(`${playDate.substring(0,4)}년 ${playDate.substring(4,6)}월 ${playDate.substring(6,8)}일\nIMAX관 오픈\n`);
+            console.log(movieNm);
+            console.log(timeTable);
+
+            // Telegram에 전송
+            sendMsg(playDate.substring(0,4) + "년 " + playDate.substring(4,6) + "월 " +
+                    playDate.substring(6,8) + "일\nIMAX관 오픈\n\n" + movieNm + "\n" + timeTable);
+
+            job.cancel();   // 스케줄러 종료
         }
         else {
             console.log("IMAX관이 열리지 않았습니다.");
