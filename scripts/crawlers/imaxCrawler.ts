c69b2941f0a7ad819827792be67263a5a1c648dc
DongminL <milkskfk12@gmail.com>
2025-07-19 18:36:30 +0900

    feat: 영화관 선택

--- scripts/crawlers/imaxCrawler.ts
+++ scripts/crawlers/imaxCrawler.ts
@@ -26,7 +26,31 @@ class ImaxCrawler extends Crawler {
                     timeout: 20000
                 } as const;
 
-                await page.goto(this.config.urls.imax + this.date, pageOption);   // CGV 용산아이파크몰점 예매 사이트 접속
+                await page.goto(this.config.urls.imax, pageOption);   // CGV 극장별 예매 사이트 접속
+                
+                const targetTheater: string = this.theater === "용아맥" ? "용산아이파크몰" : "";
+
+                // 영화관 선택
+                await page.waitForSelector('div.bottom_listCon__8g46z > ul > li');  // 렌더링 대기
+                const theaterList = await page.$$('div[class="bottom_listCon__8g46z"] > ul > li');
+                for (const li of theaterList) {
+                    const button = await li.$('button');
+                    if (!button) {
+                        continue;
+                    }
+
+                    const p = await button.$('p');
+                    if (!p) {
+                        continue;
+                    }
+
+                    const text: string|undefined = await page.evaluate(e => e.textContent?.trim(), p);
+                    if (text?.includes(targetTheater)) {
+                        await button.click();
+                        console.log("용산아이파크몰 선택");
+                        break;
+                    }
+                }
 
                 // 상영시간표 정보가 담긴 iframe으로 전환
                 const ifrmHandle: ElementHandle<HTMLIFrameElement> | null = await page.$('iframe[id="ifrm_movie_time_table"]');
@@ -42,52 +66,8 @@ class ImaxCrawler extends Crawler {
                 // IMAX관 오픈 여부에 따른 처리
                 if (imax.length > 0) {
                     let timeTable: string = ""; // 상영 시간표 및 남은 좌석수
-                    let movieNm: string = $(imax).parents('.col-times').find('.info-movie > a > strong').text().trim(); // IMAX관에서 상영하는 영화 이름
-                    let playDate: string | undefined = $(imax).parents('.type-hall').find('.info-timetable > ul > li > a').attr('data-playymd');    // 상영 날짜 (yyyymmdd)
+                    
 
-                    // uri의 date 값과 상영 날짜의 값이 다르면 종료
-                    if (playDate !== this.date) {
-                        console.log("IMAX관이 열리지 않았습니다.");
-
-                        await this.trick();   // 차단 회피
-                        await page.close();
-                        continue;
-                    }
-
-                    timeTable += (`${playDate?.substring(0, 4)}년 ${playDate?.substring(4, 6)}월 ${playDate?.substring(6, 8)}일\nIMAX 오픈\n\n`); // 상영 날짜 추가
-                    timeTable += (movieNm + "\n");  // 영화 제목 추가
-
-                    // 상영시간표 가져오기
-                    $(imax).parents('.type-hall').find('.info-timetable > ul > li')
-                    .each((i, e) => {
-                        let startTime: string | undefined = $(e).find('a').attr('data-playstarttime');  // 상영 시작 시간
-                        let endTime: string | undefined = $(e).find('a').attr('data-playendtime');  // 상영 종료 시간
-                        let seatRemainCnt: string | undefined = $(e).find('a').attr('data-seatremaincnt');  // 남은 좌석수
-                        let link: string = "http://www.cgv.co.kr" + $(e).find('a').attr('href'); // 예매하는 페이지 url
-
-                        // 예매 준비중이거나 마감 표시
-                        if ($(e).find('a').length < 1) {
-                            startTime = $(e).find('em').text();
-                            seatRemainCnt = $(e).find('span').text();
-
-                            timeTable += ("\n" + startTime +
-                                " | 남은 좌석수 : " + seatRemainCnt);
-
-                        } else if (seatRemainCnt == null || startTime == null) {
-                            startTime = $(e).find('em').text();
-                            seatRemainCnt = $(e).find('span').text();
-
-                            timeTable += ("\n" + startTime +
-                                " | 남은 좌석수 : " + seatRemainCnt +
-                                " | [예매](" + link + ")");
-
-                        } else {
-                            timeTable += ("\n" + startTime.substring(0, 2) + ":" + startTime.substring(2, 4) + " ~ " +
-                                endTime?.substring(0, 2) + ":" + endTime?.substring(2, 4) +
-                                " | 남은 좌석수 : " + seatRemainCnt +
-                                " | [예매](" + link + ")");
-                        }
-                    });
 
                     console.log(timeTable);
 
