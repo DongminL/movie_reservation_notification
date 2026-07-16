b2182ba1486815c065d133dfcdb684b3237e96d6
DongminL <milkskfk12@gmail.com>
2026-07-16 22:05:01 +0900

    fix: IPv4로만 연결하기

--- scripts/main.ts
+++ scripts/main.ts
@@ -1,5 +1,8 @@
+import net from 'node:net';
 import TelegramBot from './telegram';
 
+// Telegram Bot AggregateError 방지
+net.setDefaultAutoSelectFamily(false);
 
 const bot: TelegramBot = new TelegramBot();
 bot.setupBot();
